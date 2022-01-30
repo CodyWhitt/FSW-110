10cf1384b55c45948159988212f0d421d95a6eb9
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-01-29 20:57:38 -0600

    finished assignment 2
    null

---
+++ Week_1/Assignment2/secondassignment.js
@@ -0,0 +1,6 @@
+var greeting = document.getElementById('hello');
+
+document.getElementById('hello').innerHTML = "Successfull attempt at changing H1";
+
+console.log(greeting);
+console.log(document);
