1403c886e97b8f366b03fff87db0f8e4bac669cb
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-02-05 16:57:55 -0600

    resub

--- Week_1/Assignment2/secondassignment.js
+++ Week_1/Assignment2/secondassignment.js
@@ -1,6 +1,1 @@
-var greeting = document.getElementById('hello');
-
 document.getElementById('hello').innerHTML = "Successfull attempt at changing H1";
-
-console.log(greeting);
-console.log(document);
