5935522f979f9307428cc9f3d43fc1716e8e6227
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-02-05 20:17:47 -0600

    crud

---
+++ Week-2/Assignment 1/thing.js
@@ -0,0 +1,7 @@
+//var header = document.createElement('h1')
+//header.textcontent = 'yo its a header thats like big n struff.'
+//document.body.appendChild(header)
+
+var newP = document.createElement('p')
+newP.textcontent = 'Add this paragraph to the body of the HTML file'
+document.body.appendChild(newP)
