afdeeb849c1e1b13028d3ee752bb1705264b997f
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-02-05 22:32:57 -0600

    final touches

--- Week-2/Assignment 2/thing2.js
+++ Week-2/Assignment 2/thing2.js
@@ -1,28 +1,61 @@
+//navBar
+var navBar = document.createElement('div')
+navBar.setAttribute('class', 'topnav')
+
+var link1 = document.createElement('a')
+link1.setAttribute('href', 'https://www.google.com/')
+link1.textContent = 'Google '
+navBar.appendChild(link1)
+
+var link2 = document.createElement('a')
+link2.setAttribute('href', 'https://www.w3schools.com/')
+link2.textContent = 'W3Schools '
+navBar.appendChild(link2)
+
+var link3 = document.createElement('a')
+link3.setAttribute('href', 'https://developer.mozilla.org/en-US/')
+link3.textContent = 'Mozilla'
+navBar.appendChild(link3)
+
+document.body.appendChild(navBar)
+
+//header
 var newHeader = document.createElement('h1')
 newHeader.textContent = 'Welcome to a website'
 document.body.appendChild(newHeader)
 
+//paragraph
 var newP = document.createElement('p')
 newP.textContent = "This is a site that really dosn't have much too it."
 document.body.appendChild(newP)
 
+//list
 var newList = document.createElement('ol')
-newList.setAttribute('id', 'newList')
-newList.textContent = 'Three Items'
-document.body.appendChild(newList)
+newList.textContent = 'List of things it needed to pass'
 
 var one = document.createElement('li')
-one.textContent = '1'
+one.textContent = 'an h1 tag'
 newList.appendChild(one)
 
 var two = document.createElement('li')
-two.textContent = '2'
+two.textContent = 'a p tag'
 newList.appendChild(two)
 
 var three = document.createElement('li')
-three.textContent = '3'
+three.textContent = 'a list'
 newList.appendChild(three)
 
-var newP = document.createElement('footer')
-newP.textContent = "Heres a footer."
-document.body.appendChild(newP)
+var four = document.createElement('li')
+four.textContent = 'a footer'
+newList.appendChild(four)
+
+var five = document.createElement('li')
+five.textContent = 'navbar w/ links'
+newList.appendChild(five)
+
+document.body.appendChild(newList)
+
+//footer
+var newFooter = document.createElement('footer')
+newFooter.textContent = "Heres a footer."
+document.body.appendChild(newFooter)
