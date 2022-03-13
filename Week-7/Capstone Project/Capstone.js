0b52f8c013ffea619856edae08ddaa1152240d0c
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-03-13 15:09:24 -0500

    Capstone submission

---
+++ Week-7/Capstone Project/Capstone.js
@@ -0,0 +1,23 @@
+submit = document.getElementById("submit")
+task = document.getElementById("task")
+description = document.getElementById("description")
+time = document.getElementById("time")
+form = document.getElementById("form")
+output = document.getElementById("output")
+counter = 0
+
+submit.addEventListener("click", function(e) {
+    e.preventDefault()
+    final = document.createElement('li')
+    deleteButton = document.createElement("button")
+    deleteButton.setAttribute("id", counter)
+    counter = (counter+1)
+    final.innerHTML = task.value + ": " + description.value + " @ " + time.value + " "
+    output.appendChild(final)
+    final.appendChild(deleteButton)
+    deleteButton.innerHTML = "delete"
+    deleteButton.onclick = function() {
+        this.parentElement.remove()
+    };
+    console.log("Item Submitted")
+})
