700a3b14d5fdde927abb75a150b967793a6e305f
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-03-27 18:40:00 -0500

    small changes

--- Week-3/2/W3A2.js
+++ Week-3/2/W3A2.js
@@ -4,7 +4,7 @@ for (i = 0; i <5; i++) {
     words.textContent = 'Hello World';
     words.style.fontSize = '20px'
     words.style.fontWeight = 'lighter'
-    words.style.fontFamilty = 'sans-serif'
+    words.style.fontFamily = 'sans-serif'
     words.style.backgroundColor = 'cornFlowerBlue'
     words.classList.add("border");
     document.body.appendChild(words);
